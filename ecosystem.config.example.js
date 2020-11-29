8ce87d2147655b2394167ab9172323a4cab71dd5
José Lucena <lucena.jh@gmail.com>
2020-11-29 01:25:29 -0300

    Fix ecosystem

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -1,12 +1,12 @@
 module.exports = {
     apps: [{
         name: "react-url-shortener",
-        script: "./scripts/start.js",
+        script: "serve",
         env: {
-            PM2_SERVE_PATH: '.',
+            PM2_SERVE_PATH: 'build',
             PM2_SERVE_PORT: 3000,
             PM2_SERVE_SPA: 'true',
-            PM2_SERVE_HOMEPAGE: './index.html',
+            PM2_SERVE_HOMEPAGE: '/index.html',
             NODE_ENV: "production",
             REACT_APP_API_HOST: "localhost",
             REACT_APP_API_PORT: 3333,
