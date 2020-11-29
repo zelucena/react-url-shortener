c1d4a67a29d82a4514b3b13644d0a43fc6613715
José Lucena <lucena.jh@gmail.com>
2020-11-29 01:15:06 -0300

    Update example for ecosystem

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -3,8 +3,12 @@ module.exports = {
         name: "react-url-shortener",
         script: "./scripts/start.js",
         env: {
+            PM2_SERVE_PATH: '.',
+            PM2_SERVE_PORT: 3000,
+            PM2_SERVE_SPA: 'true',
+            PM2_SERVE_HOMEPAGE: './index.html',
             NODE_ENV: "production",
-            REACT_APP_API_HOST: "127.0.0.1",
+            REACT_APP_API_HOST: "localhost",
             REACT_APP_API_PORT: 3333,
             REACT_APP_API_ADDRESS: "http://$REACT_APP_API_HOST:$REACT_APP_API_PORT",
         },
